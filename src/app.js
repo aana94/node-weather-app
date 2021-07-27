e29fd2d691d5540365d8841f553643ec470fcf20
achhli <aanchalgrover1994@gmail.com>
2021-07-27 08:44:20 +0530

    initial commit

---
+++ src/app.js
@@ -0,0 +1,75 @@
+const path = require('path')
+const request = require('request');
+const express = require('express');
+const hbs = require('hbs')
+
+const app = express();
+const  publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+const geocode = require('../utils/geocode')
+const forecast = require('../utils/forecast')
+
+const address = process.argv[2]
+
+
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
+        name: 'Aanchal'
+    });
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Weather',
+        name: 'Aanchal Help'
+    });
+})
+
+app.get('/weather', (req, res) => {
+    let address = req.query.address
+    if(!address){
+        return res.send({
+            error: 'Please provide the address for location'
+        })
+    }
+    geocode(address, (error, {latitude, longitude, location} = {}) => {
+        if(error) {
+            return res.send(error);
+        }
+        forecast(latitude, longitude, (error, forecastData) => {
+            if(error) {
+               return res.send(error);
+            }
+            res.send({
+                location,
+                forecast: forecastData,
+                address: address
+            });
+            // res.send();
+        })
+    })
+})
+
+app.get('/help/*', (req, res) => {
+    res.render('404',{
+        text: 'Help article not found'
+    });
+})
+
+app.get('*', (req, res) => {
+    res.render('404',{
+        text: 'My 404 page'
+    });
+})
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000.')
+})
